c7ff2cf349a0f32ac8cf50ce62fa50ba2dca0d0d
Rodrigo Gonçalves <70276841+RodrigoGoncalves24@users.noreply.github.com>
2025-09-09 14:27:32 -0300

    Projeto finalizado

--- src/main/java/com/rodrigo/biblioteca/Service/ClienteService.java
+++ src/main/java/com/rodrigo/biblioteca/Service/ClienteService.java
@@ -2,24 +2,24 @@ package com.rodrigo.biblioteca.Service;
 
 import com.rodrigo.biblioteca.ConnectionFactory;
 import com.rodrigo.biblioteca.DAO.ClienteDAO;
-import com.rodrigo.biblioteca.Domain.Cliente;
+import com.rodrigo.biblioteca.DTO.ClienteResumo;
 import com.rodrigo.biblioteca.Domain.Emprestimo;
 import com.rodrigo.biblioteca.RegraDeNegocioException;
 
 import java.sql.Connection;
+
 import java.util.Set;
 
 
 public class ClienteService {
-    // Regras de negócio do usuário
     private ConnectionFactory connection;
-    private EmprestimoService serviceEmprestimo;
+    private EmprestimoService serviceEmprestimo = new EmprestimoService();
 
     public ClienteService() {
         this.connection = new ConnectionFactory();
     }
 
-    public Set<Cliente> listarCliente() {
+    public Set<ClienteResumo> listarCliente() {
         Connection conn = connection.recuperarConecxao();
         return new ClienteDAO(conn).listarCliente();
     }
@@ -27,31 +27,19 @@ public class ClienteService {
     public void cadastrarCliente(String nome, String email, String cpf) {
         Connection conn = connection.recuperarConecxao();
 
-        if (nome.contains("delete") || email.contains("delete")) {
-            throw new RegraDeNegocioException("Aqui não tico tico");
-        }
-        //Verifica se cpf é valido
-//        if(cpf.length() != 11){
-//            throw new RegraDeNegocioException("CPF inválido! Por favor, verifique e tente novamente!");
-//        }
-
-        Cliente c = new ClienteDAO(conn).buscarClinete(cpf);
-
-        //Verifica se já não existe esse cliente na base
-        if (c != null) {
+        //Verifica se cliente já esta cadastrado
+        if (clienteCadastrado(cpf)) {
             throw new RegraDeNegocioException("Cliente já cadastrado");
         }
 
-        conn = connection.recuperarConecxao(); // Péssimo prática, ajustar posteriormente
         new ClienteDAO(conn).cadastraCliente(nome, email, cpf);
-
-
     }
 
     // Antes de exluir, verificar se há livros com o cliente ou se ele está ok para ser exlcuido
     public void excluirCliente(String cpf) {
         Connection conn = connection.recuperarConecxao();
-        Set<Emprestimo> emprestimoPorCliente = serviceEmprestimo.emprestimoPorCliente(cpf);
+        Set<Emprestimo> emprestimoPorCliente;
+        emprestimoPorCliente = serviceEmprestimo.emprestimoPorCliente(cpf);
 
         if (!temEmprestimo(emprestimoPorCliente)) {
             throw new RegraDeNegocioException("O cliente informado possui livros em empréstimo");
@@ -71,11 +59,15 @@ public class ClienteService {
     public int idCliente(String cpf){
         Connection conn = connection.recuperarConecxao();
 
-        if(cpf.contains("delete") || cpf.contains("null")){
-            throw new RegraDeNegocioException("Aqui não tico tico");
-        }
-
         return new ClienteDAO(conn).idCliente(cpf);
+    }
+
+    private boolean clienteCadastrado(String cpf){
+        Connection conn = connection.recuperarConecxao();
+
+        ClienteResumo c = new ClienteDAO(conn).buscarCliente(cpf);
+
+        return c != null;
     }
 
 }
